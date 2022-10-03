8d7affb05ea9004617124353aada81467d19466e
RAGHAV172000 <87511875+RAGHAV172000@users.noreply.github.com>
2022-10-03 06:36:59 +0530

    first commit

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3be8bcd9330069",
+    pass: "d2d74eaa8551de"
+  }
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
